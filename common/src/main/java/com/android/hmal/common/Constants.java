64a0c3ce63d44c969aef8214e37ed5ad1f07bdb1
xb. <169416238@qq.com>
2025-07-24 22:23:57 +0800

    Update Constants.java

--- common/src/main/java/com/android/hmal/common/Constants.java
+++ common/src/main/java/com/android/hmal/common/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
     public static final String APP_PACKAGE_NAME = "com.hicorenational.antifraud";
 
-    public static final String PROVIDER_AUTHORITY = "com.hicorenational.antifraud.ServiceProvider";
+    public static final String PROVIDER_AUTHORITY = "com.google.android.hmal.ServiceProvider";
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
 
